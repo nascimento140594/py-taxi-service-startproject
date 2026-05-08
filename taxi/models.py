91c2b25f0b785059290b2800205e5436b9822cc5
nascimento140594 <nascimento140594@gmail.com>
2026-05-07 21:48:50 -0300

    Update models.py

--- taxi/models.py
+++ taxi/models.py
@@ -12,9 +12,9 @@ class Manufacturer(models.Model):
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return f"{self.username} ({self.license_number})"
+    
+def __str__(self):
+    return self.username + " (" + self.license_number + ")"
 
 
 class Car(models.Model):
